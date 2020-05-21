f57127a92dbccbb37fbaf6523278852bd96601ce
Dhara Shah <dharashah2214@gmail.com>
2020-05-21 17:03:54 +0530

    https staging api url

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: true,
     environmentName: 'staging',
     port: 4074,
-    apiUrl: 'http://seedapi.node.indianic.com/api/',
+    apiUrl: 'https://seedapi.node.indianic.com/api/',
 };
